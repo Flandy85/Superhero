4c919e137a3c216c4260535e1724b3c16dab851e
Anders Gustavsson <gustavsson09@hotmail.com>
2018-02-05 08:50:04 +0000

    Created 2 new files for webpages.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  # Controller for team page
+  def team #Method team
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  # Root routes for webpage
+  root 'home#index'
+  #get 'home/index' # Old/start route
+  get 'home/team'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
