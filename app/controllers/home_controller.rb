3655547cda1bbcfea3950424fc899376018b463f
Anders Gustavsson <gustavsson09@hotmail.com>
2018-02-13 08:51:37 +0000

    Fixed problem with avatar not showing, forgot src attribute in img tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,39 +1,39 @@
 class HomeController < ApplicationController
   # before anything else happens, common:content method, accessable only for index and team
-  before_action :common_content, :only => [:index, :team] 
+  before_action :common, :only => [:index, :team] 
   
- def common_content
+ def common
      # Variables with fake generator items
    @hero_name = Faker::Superhero.name
    @hero_power = Faker::Superhero.power
    @hero_prefix = Faker::Superhero.prefix
-   @hero_suffix = Faker::Superhero.suffix 
-   @hero_descriptor = Faker::Superhero.descriptor
+   # @hero_suffix = Faker::Superhero.suffix 
+   # @hero_descriptor = Faker::Superhero.descriptor
    @hero_corp_title = Faker::Name.title 
    @hero_corp_power = Faker::Company.bs
    @hero_corp_power = @hero_corp_power.titleize
    
    # Variables for randomnize avatars, avtar-backgrounds.
    @rand_num = rand(1..1000)
-   @set_number = rand(1..4) 
+   @set_num = rand(1..4)
    @bg = rand(1..2)
    
    # if statment for randominze avatar picture
-   if @set_number == 1
-     @set = "set1"
-    elsif @set_number == 2
-      @set = "set2"
-    elsif @set_number == 3
-      @set = "set3"
-    else 
-      @set = "set4"
+   if @set_num == 1
+       @set = 'set1'
+    elsif @set_num == 2
+        @set = 'set2'
+    elsif @set_num == 3
+        @set = 'set3'
+    else
+        @set = 'set4'
    end
    
    # if statement for randominze background i avatar picture
    if @bg == 1
-     @background = "bg1"
+        @background = 'bg1'
     else
-      @background = "bg2"
+        @background = 'bg2'
    end
    # Varaible with url from robothash.org, for customizing url 
    @avatar = Faker::Avatar.image(@rand_num, "300x300", "png", @set, @background)
@@ -44,7 +44,7 @@ class HomeController < ApplicationController
   end
   
   # Controller for team page
-  def team #Method team
+  def team 
     @team_number = params[:team_number].to_i
   end
 end
